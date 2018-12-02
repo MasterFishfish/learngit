3be573028b154afc66ff0f264477a8ba77060abb
MasterFishfish <tusimplefishfish@foxmail.com>
2018-12-02 20:35:50 +0800

    :

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 import tensorflow as tf
 import numpy as np
+import time
 
 if __name__ == '__main__':
     print("aaa")
+    nowTime = time.localtime()
+    date = time.strftime("%Y-%m-%d %H:%M:%S", nowTime)
+    with open('./BOM.txt', 'a') as f:
+        f.write("\n" + date)
